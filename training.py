8bb8ddbc28dcbcb50a47ebd500774fe3994aaa31
shyGeeks <157801061+shyGeeks@users.noreply.github.com>
2024-01-27 10:24:18 +0500

    beginning

--- training.py
+++ training.py
@@ -1,1 +1,9 @@
-#nothin yet
+#kutubxonlar importi
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
+
+dataset_file_path = 'Crop_recommendation.xlsx'  # faylni o`qish
+df = pd.read_excel(dataset_file_path)
+# df.head()
