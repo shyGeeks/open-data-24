f6cc0f4b68ed5124522316483fb27cf7dc5ee89a
shyGeeks <157801061+shyGeeks@users.noreply.github.com>
2024-01-27 11:06:03 +0500

    Update training.py

--- training.py
+++ training.py
@@ -7,3 +7,22 @@ from sklearn.metrics import accuracy_score, classification_report
 dataset_file_path = 'Crop_recommendation.xlsx'  # faylni o`qish
 df = pd.read_excel(dataset_file_path)
 # df.head()
+
+
+X = df.iloc[:, :-1]  # Features
+y = df.iloc[:, -1]   # Target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+model = RandomForestClassifier(random_state=42)
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+# modelni baholash 
+# accuracy = accuracy_score(y_test, y_pred)
+# print(f"Accuracy: {accuracy}")
+
+new_data = [[12, 50, 46, 19.770462, 230.319644, 7.038096, 150.655537]]
+prediction = model.predict(new_data)
+print(prediction)
